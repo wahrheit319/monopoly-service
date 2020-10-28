53f4745697190abb27659861e5896de8928eb905
kvlinden <kvlinden@calvin.edu>
2020-10-28 11:43:13 -0400

    updates for unit 9 rest services

--- sql/monopoly.sql
+++ sql/monopoly.sql
@@ -12,12 +12,12 @@ DROP TABLE IF EXISTS Player;
 
 -- Create the schema.
 CREATE TABLE Game (
-	ID integer PRIMARY KEY, 
+	ID SERIAL PRIMARY KEY,
 	time timestamp
 	);
 
 CREATE TABLE Player (
-	ID integer PRIMARY KEY, 
+	ID SERIAL PRIMARY KEY,
 	email varchar(50) NOT NULL,
 	name varchar(50)
 	);
@@ -34,13 +34,13 @@ GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
 
 -- Add sample records.
-INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
-INSERT INTO Game VALUES (2, '2006-06-28 13:20:00');
-INSERT INTO Game VALUES (3, '2006-06-29 18:41:00');
+INSERT INTO Game(time) VALUES ('2006-06-27 08:00:00');
+INSERT INTO Game(time) VALUES ('2006-06-28 13:20:00');
+INSERT INTO Game(time) VALUES ('2006-06-29 18:41:00');
 
-INSERT INTO Player(ID, email) VALUES (1, 'me@calvin.edu');
-INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
-INSERT INTO Player VALUES (3, 'dog@gmail.edu', 'Dogbreath');
+INSERT INTO Player(email) VALUES ('me@calvin.edu');
+INSERT INTO Player(email, name) VALUES ('king@gmail.edu', 'The King');
+INSERT INTO Player(email, name) VALUES ('dog@gmail.edu', 'Dogbreath');
 
 INSERT INTO PlayerGame VALUES (1, 1, 0.00);
 INSERT INTO PlayerGame VALUES (1, 2, 0.00);
