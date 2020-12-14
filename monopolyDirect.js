75bf1be6de2962a0d402cf4fe2ef65ad846ee6a4
kvlinden <kvlinden@calvin.edu>
2020-12-14 13:53:42 -0500

    adding SSL flag

--- monopolyDirect.js
+++ monopolyDirect.js
@@ -21,6 +21,7 @@ const db = pgp({
     database: process.env.DB_USER,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    // For SSL, see: https://stackoverflow.com/questions/22301722/ssl-for-postgresql-connection-nodejs
     ssl: true,
 });
 
