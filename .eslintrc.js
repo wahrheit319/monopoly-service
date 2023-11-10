806ff3bcdf9437c8df04648c5a4f714fb4496b32
kvlinden <kvlinden@calvin.edu>
2023-11-10 12:18:57 -0500

    integrating eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    root: true,
+    rules: {
+        "no-undef": "off",
+        "indent": ["error", 4],
+        "prettier/prettier": "off",
+    },
+    "env": {
+        "es6": true
+    },    
+};
