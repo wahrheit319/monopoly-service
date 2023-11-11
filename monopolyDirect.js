e5b3f51bc6bff4d0ac6768bfb0af857b35b1c456
kvlinden <kvlinden@calvin.edu>
2023-11-11 15:24:10 -0500

    moving to eslint enforcement

--- monopolyDirect.js
+++ monopolyDirect.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * This module implements direct, Android-to-Postgres access to the Monopoly DB.
  * The database is hosted on ElephantSQL.
@@ -15,21 +16,22 @@
 
 // Set up the database connection.
 const pgp = require('pg-promise')();
+
 const db = pgp({
-    host: process.env.DB_SERVER,
-    port: process.env.DB_PORT,
-    database: process.env.DB_USER,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    // For SSL, see: https://stackoverflow.com/questions/22301722/ssl-for-postgresql-connection-nodejs
-    ssl: true,
+  host: process.env.DB_SERVER,
+  port: process.env.DB_PORT,
+  database: process.env.DB_USER,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  // For SSL, see: https://stackoverflow.com/questions/22301722/ssl-for-postgresql-connection-nodejs
+  ssl: true,
 });
 
 // Send the SQL command directly to Postgres.
-db.many("SELECT * FROM Player")
-    .then(function (data) {
-        console.log(data);
-    })
-    .catch(function (error) {
-        console.log('ERROR:', error)
-    });
+db.many('SELECT * FROM Player')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log('ERROR:', error);
+  });
